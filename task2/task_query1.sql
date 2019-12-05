b7ff8a20337f6b23f7151355da90829c48d0ced7
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2019-12-05 21:46:09 +0300

    sql 1

--- task2/task_query1.sql
+++ task2/task_query1.sql
@@ -1,9 +1,15 @@
-SELECT `a`.`worker_id`, max(`a`.`money_sum`), `a`.`date`
-from (
-    SELECT FROM_UNIXTIME(`timestamp`, '%Y-%m-%d') as `date` , sum(`money`) as `money_sum`,`worker_id`
-
-FROM `mytable` 
-group by `worker_id`, `date`
-) as `a`
-
-group by `a`.`date
+SELECT `a`.`worker_id`,
+       max(`a`.`money_sum`),
+       `a`.`date`
+FROM
+  (SELECT FROM_UNIXTIME(`timestamp`, '%Y-%m-%d') AS `date`,
+          sum(`money`) AS `money_sum`,
+          `worker_id`
+   FROM
+     (SELECT *
+      FROM `mytable`
+      UNION ALL SELECT *
+      FROM `mytable5`) AS `united_table`
+   GROUP BY `worker_id`,
+            `date`) AS `a`
+GROUP BY `a`.`date`
